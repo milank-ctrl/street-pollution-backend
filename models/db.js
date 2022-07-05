2ad0994b884769f6cc60a093b37898d684695d2d
milank-ctrl <kocza.milan@gmail.com>
2022-07-05 18:20:55 +0200

    adding readme and change mongodb schema

--- models/db.js
+++ models/db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const monitorSchema = mongoose.Schema({
     sample_datetime: Date,
     pm10: Number,
-    pm25: Number 
+    pm25: Number,
+    istest: Number
 })
 
 module.exports = mongoose.model('monitor', monitorSchema);
